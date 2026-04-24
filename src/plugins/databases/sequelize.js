38282726835fd22eaabffeec208f55b34b41249a
Tania Dzumba <tania.dzumba@safetyio.com>
2026-04-24 14:28:28 +0200

    Set up Sequelize and MySQL connection in Fastify app

--- src/plugins/databases/sequelize.js
+++ src/plugins/databases/sequelize.js
@@ -1,14 +1,28 @@
 import fp from "fastify-plugin";
+import { Sequelize } from "sequelize";
 
 async function sequelizePlugin(fastify, config) {
   let mysqlStatus = "disconnected";
 
   // TODO: Connect to MySQL via Sequelize and update the status
+  try{
+    const sequelize = new Sequelize(config.uri, config.options);
+    await sequelize.authenticate();
+    fastify.log.info("Connected to MySQL");
+    mysqlStatus = "connected";
+    fastify.decorate("sequelize", sequelize);
+      
+  } catch(err){
+
+    fastify.log.error("failed to connect to MySQL");
+    throw err;
+  }
   fastify.decorate("mysqlStatus", () => mysqlStatus);
 
   // Graceful shutdown
   fastify.addHook("onClose", async (fastifyInstance, done) => {
     mysqlStatus = "disconnected";
+    await sequelize.close();
     // TODO: Close Sequelize connection
     done();
   });
